a3fe043cec5119282ff4ff4c4d2366889b0bd66f
Matthew <smatthew120@gmail.com>
2022-11-21 12:12:32 -0500

    Added teacherAddHomework.html and teacherAddQuiz.html. Also added JavaScript functionalities for teacherAddQuiz.html (see JS folder).

---
+++ HTML Pages/JS/addQuizQuestion.js
@@ -0,0 +1,51 @@
+divName = 2;
+qName = 1;
+sName = 1;
+mName = 1;
+
+function addQuestion() {
+
+  var newQuestion = document.getElementById("questionSection");
+  var newName = "Question" + qName; // CHANGES THE NAME OF THE ELEMENT SO THAT ALL QUESTIONS ARE INDEPENDENT OF ONE ANOTHER
+
+  newQuestion.innerHTML += "<label>Question: </label> <br />"; // ADDS THE QUESTION LABEL 
+  newQuestion.innerHTML += "<input type='radio' name='" + newName +  "' value='1' onclick=" + "addSingle('choice" + divName +"') />Multiple Choice (single)<br />"; // ADDS THE FIRST RADIO BUTTON  
+  newQuestion.innerHTML += "<input type='radio' name='" + newName + "' value='2' onclick=" + "addMultiple('choice" + divName +"') />Multiple Choice (multiple)<br />"; // ADDS THE SECOND RADIO BUTTON        
+  newQuestion.innerHTML += "<input type='radio' name='"+ newName + "' value='3' onclick=" + "addShort('choice" + divName + "') />Short Answer<br />"; // ADDS THE THIRD RADIO BUTTON 
+  newQuestion.innerHTML += "<div id='choice" + divName + "'></>"; // NEW DIV 
+
+  qName += 1;
+  divName += 1;
+
+}
+
+//// FOR SELECTING MULTIPLE CHOICE QUESTIONS OR SHORT ANSWER /////
+
+function addSingle(div) {
+    var newQuestion = document.querySelector('#' + div);
+
+    newQuestion.innerHTML = "<label>Multiple Choices (single): </label><br />"; // ADDS A QUESTION LABEL 
+    newQuestion.innerHTML += "Choice A: <input type='text' /><br />"; // ADDS THE INPUT TEXT ELEMENT
+    newQuestion.innerHTML += "Choice B: <input type='text' /><br />"; // ADDS THE INPUT TEXT ELEMENT
+    newQuestion.innerHTML += "Choice C: <input type='text' /><br />"; // ADDS THE INPUT TEXT ELEMENT
+    newQuestion.innerHTML += "Choice D: <input type='text' /><br />"; // ADDS THE INPUT TEXT ELEMENT
+
+}
+
+function addMultiple(div) {
+    var newQuestion = document.querySelector('#' + div);
+
+    newQuestion.innerHTML = "<label>Multiple Choices (multiple): </label><br />"; // ADDS A QUESTION LABEL 
+    newQuestion.innerHTML += "Choice A: <input type='text' /><br />"; // ADDS THE INPUT TEXT ELEMENT
+    newQuestion.innerHTML += "Choice B: <input type='text' /><br />"; // ADDS THE INPUT TEXT ELEMENT
+    newQuestion.innerHTML += "Choice C: <input type='text' /><br />"; // ADDS THE INPUT TEXT ELEMENT
+    newQuestion.innerHTML += "Choice D: <input type='text' /><br />"; // ADDS THE INPUT TEXT ELEMENT
+
+}
+
+function addShort(div) {
+    var newQuestion = document.querySelector('#' + div);
+
+    newQuestion.innerHTML = "<label>Short Answer: </label><br />"; // ADDS A QUESTION LABEL 
+    newQuestion.innerHTML += "<input type='text' name='shortAnswer' /><br />"; // ADDS THE INPUT TEXT ELEMENT
+}
